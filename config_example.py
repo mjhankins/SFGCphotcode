48727025027e96dac93869de8eb15c9d931375ca
mjhankins <mjhankins44@gmail.com>
2021-07-02 12:02:24 -0500

    changes to file names in example config

--- config_example.py
+++ config_example.py
@@ -50,8 +50,8 @@ class field():
         self._registry.append(self)
 
 #file names for field1 
-file25='F0592_FO_IMA_0701891_FORF253_CAL_0095-0118_FIELD_1.fits'
-file37='F0592_FO_IMA_0701891_FORF371_CAL_0095-0118_FIELD_1.fits'
+file25='F0592_FO_IMA_0701891_FORF253_CAL_0095-0118.fits'
+file37='F0592_FO_IMA_0701891_FORF371_CAL_0095-0118.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -65,8 +65,8 @@ m2lims=[[280,360,0,370],[0,350,0,50],[200,350,300,370]], #list of manually defin
 m3lims=None) #additional mask if needed in the form [y1,y2,x1,x2]
 
 #file names for field2 
-file25='F0592_FO_IMA_0701892_FORF253_CAL_0120-0141_FIELD_2.fits'
-file37='F0592_FO_IMA_0701892_FORF371_CAL_0120-0141_FIELD_2.fits'
+file25='F0592_FO_IMA_0701892_FORF253_CAL_0120-0141.fits'
+file37='F0592_FO_IMA_0701892_FORF371_CAL_0120-0141.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -80,8 +80,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field3 
-file25='F0595_FO_IMA_0701893_FORF253_CAL_0283-0303_FIELD_3.fits'
-file37='F0595_FO_IMA_0701893_FORF371_CAL_0283-0303_FIELD_3.fits'
+file25='F0595_FO_IMA_0701893_FORF253_CAL_0283-0303.fits'
+file37='F0595_FO_IMA_0701893_FORF371_CAL_0283-0303.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -95,8 +95,8 @@ m2lims=[[0,150,250,300]],
 m3lims=None)
 
 #file names for field5 
-file25='F0595_FO_IMA_0701895_FORF253_CAL_0259-0282_FIELD_5.fits'
-file37='F0595_FO_IMA_0701895_FORF371_CAL_0259-0282_FIELD_5.fits'
+file25='F0595_FO_IMA_0701895_FORF253_CAL_0259-0282.fits'
+file37='F0595_FO_IMA_0701895_FORF371_CAL_0259-0282.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -110,8 +110,8 @@ m2lims=[[0,330,250,300]],
 m3lims=[[0,330,0,70]])
 
 #file names for field6
-file25='F0593_FO_IMA_0701896_FORF253_CAL_0099-0124_Field_6.fits'
-file37='F0593_FO_IMA_0701896_FORF371_CAL_0099-0124_Field_6.fits'
+file25='F0593_FO_IMA_0701896_FORF253_CAL_0099-0124.fits'
+file37='F0593_FO_IMA_0701896_FORF371_CAL_0099-0124.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -125,8 +125,8 @@ m2lims=[[290,370,0,370],[0,370,290,380],[0,30,0,370],[0,370,0,50]],
 m3lims=None)
 
 #file names for field7 
-file25='F0593_FO_IMA_0701897_FORF253_CAL_0125-0136_Field_7.fits'
-file37='F0593_FO_IMA_0701897_FORF371_CAL_0125-0136_Field_7.fits'
+file25='F0593_FO_IMA_0701897_FORF253_CAL_0125-0136.fits'
+file37='F0593_FO_IMA_0701897_FORF371_CAL_0125-0136.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -140,8 +140,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field8 
-file25='F0591_FO_IMA_0701898_FORF253_CAL_0006-0038_Field_8.fits'
-file37='F0591_FO_IMA_0701898_FORF371_CAL_0006-0038_Field_8.fits'
+file25='F0591_FO_IMA_0701898_FORF253_CAL_0006-0038.fits'
+file37='F0591_FO_IMA_0701898_FORF371_CAL_0006-0038.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -155,8 +155,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field9 
-file25='F0588_FO_IMA_0701899_FORF253_CAL_0042-0081_Field_9.fits'
-file37='F0588_FO_IMA_0701899_FORF371_CAL_0042-0081_Field_9.fits'
+file25='F0588_FO_IMA_0701899_FORF253_CAL_0042-0081.fits'
+file37='F0588_FO_IMA_0701899_FORF371_CAL_0042-0081.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -170,8 +170,8 @@ m2lims=[[0,230,0,40],[0,50,0,50],[0,300,280,320]],
 m3lims=None)
 
 #file names for field10 
-file25='F0588_FO_IMA_07018910_FORF253_CAL_0082-0111_Field_10.fits'
-file37='F0588_FO_IMA_07018910_FORF371_CAL_0082-0111_Field_10.fits'
+file25='F0588_FO_IMA_07018910_FORF253_CAL_0082-0111.fits'
+file37='F0588_FO_IMA_07018910_FORF371_CAL_0082-0111.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -185,8 +185,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field11
-file25='F0588_FO_IMA_07018911_FORF253_CAL_0114-0124_Field_11.fits'
-file37='F0588_FO_IMA_07018911_FORF371_CAL_0114-0124_Field_11.fits'
+file25='F0588_FO_IMA_07018911_FORF253_CAL_0114-0124.fits'
+file37='F0588_FO_IMA_07018911_FORF371_CAL_0114-0124.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -200,8 +200,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field12
-file25='F0591_FO_IMA_07018935_FORF253_CAL_0039-0061_Field_12.fits'
-file37='F0591_FO_IMA_07018935_FORF371_CAL_0039-0061_Field_12.fits'
+file25='F0591_FO_IMA_07018935_FORF253_CAL_0039-0061.fits'
+file37='F0591_FO_IMA_07018935_FORF371_CAL_0039-0061.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -215,8 +215,8 @@ m2lims=[[0,50,0,350],[0,350,0,40],[300,350,40,80]],
 m3lims=None)
 
 #file names for field13
-file25='F0588_FO_IMA_07018936_FORF253_CAL_0008-0041_Field_13.fits'
-file37='F0588_FO_IMA_07018936_FORF371_CAL_0008-0041_Field_13.fits'
+file25='F0588_FO_IMA_07018936_FORF253_CAL_0008-0041.fits'
+file37='F0588_FO_IMA_07018936_FORF371_CAL_0008-0041.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -230,8 +230,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field A
-file25='F0588_FO_IMA_07018912_FORF253_CAL_0301-0325_Field_A.fits'
-file37='F0588_FO_IMA_07018912_FORF371_CAL_0301-0325_Field_A.fits'
+file25='F0588_FO_IMA_07018912_FORF253_CAL_0301-0325.fits'
+file37='F0588_FO_IMA_07018912_FORF371_CAL_0301-0325.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -245,8 +245,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field B
-file25='F0591_FO_IMA_07018913_FORF253_CAL_0088-0114_Field_B.fits'
-file37='F0591_FO_IMA_07018913_FORF371_CAL_0088-0114_Field_B.fits'
+file25='F0591_FO_IMA_07018913_FORF253_CAL_0088-0114.fits'
+file37='F0591_FO_IMA_07018913_FORF371_CAL_0088-0114.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -260,8 +260,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field C
-file25='F0588_FO_IMA_07018914_FORF253_CAL_0326-0348_Field_C.fits'
-file37='F0588_FO_IMA_07018914_FORF371_CAL_0326-0348_Field_C.fits'
+file25='F0588_FO_IMA_07018914_FORF253_CAL_0326-0348.fits'
+file37='F0588_FO_IMA_07018914_FORF371_CAL_0326-0348.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -275,8 +275,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field D
-file25='F0592_FO_IMA_07018915_FORF253_CAL_0276-0303_Field_D.fits'
-file37='F0592_FO_IMA_07018915_FORF371_CAL_0276-0303_Field_D.fits'
+file25='F0592_FO_IMA_07018915_FORF253_CAL_0276-0303.fits'
+file37='F0592_FO_IMA_07018915_FORF371_CAL_0276-0303.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -290,8 +290,8 @@ m2lims=[[200,250,0,50]],
 m3lims=None)
 
 #file names for field E
-file25='F0590_FO_IMA_07018916_FORF253_CAL_0142-0161_Field_E.fits'
-file37='F0590_FO_IMA_07018916_FORF371_CAL_0142-0161_Field_E.fits'
+file25='F0590_FO_IMA_07018916_FORF253_CAL_0142-0161.fits'
+file37='F0590_FO_IMA_07018916_FORF371_CAL_0142-0161.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -305,8 +305,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field F
-file25='F0592_FO_IMA_07018917_FORF253_CAL_0068-0094_Field_F.fits'
-file37='F0592_FO_IMA_07018917_FORF371_CAL_0068-0094_Field_F.fits'
+file25='F0592_FO_IMA_07018917_FORF253_CAL_0068-0094.fits'
+file37='F0592_FO_IMA_07018917_FORF371_CAL_0068-0094.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -320,8 +320,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field G
-file25='F0590_FO_IMA_07018918_FORF253_CAL_0117-0141_Field_G.fits'
-file37='F0590_FO_IMA_07018918_FORF371_CAL_0117-0141_Field_G.fits'
+file25='F0590_FO_IMA_07018918_FORF253_CAL_0117-0141.fits'
+file37='F0590_FO_IMA_07018918_FORF371_CAL_0117-0141.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -335,8 +335,8 @@ m2lims=[[0,100,0,250],[0,90,250,350],[350,380,0,200],[150,230,300,380]],
 m3lims=None)
 
 #file names for field H
-file25='F0589_FO_IMA_07018919_FORF253_CAL_0017-0049_Field_H.fits'
-file37='F0589_FO_IMA_07018919_FORF371_CAL_0017-0049_Field_H.fits'
+file25='F0589_FO_IMA_07018919_FORF253_CAL_0017-0049.fits'
+file37='F0589_FO_IMA_07018919_FORF371_CAL_0017-0049.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -350,8 +350,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field I
-file25='F0592_FO_IMA_07018920_FORF253_CAL_0005-0036_Field_I.fits'
-file37='F0592_FO_IMA_07018920_FORF371_CAL_0005-0036_Field_I.fits'
+file25='F0592_FO_IMA_07018920_FORF253_CAL_0005-0036.fits'
+file37='F0592_FO_IMA_07018920_FORF371_CAL_0005-0036.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -365,8 +365,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field K
-file25='F0590_FO_IMA_07018922_FORF253_CAL_0087-0116_Field_K.fits'
-file37='F0590_FO_IMA_07018922_FORF371_CAL_0087-0116_Field_K.fits'
+file25='F0590_FO_IMA_07018922_FORF253_CAL_0087-0116.fits'
+file37='F0590_FO_IMA_07018922_FORF371_CAL_0087-0116.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -380,8 +380,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field L
-file25='F0589_FO_IMA_07018923_FORF253_CAL_0050-0079_Field_L.fits'
-file37='F0589_FO_IMA_07018923_FORF371_CAL_0050-0079_Field_L.fits'
+file25='F0589_FO_IMA_07018923_FORF253_CAL_0050-0079.fits'
+file37='F0589_FO_IMA_07018923_FORF371_CAL_0050-0079.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -395,8 +395,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field M
-file25='F0593_FO_IMA_07018924_FORF253_CAL_0071-0098_Field_M.fits'
-file37='F0593_FO_IMA_07018924_FORF371_CAL_0071-0098_Field_M.fits'
+file25='F0593_FO_IMA_07018924_FORF253_CAL_0071-0098.fits'
+file37='F0593_FO_IMA_07018924_FORF371_CAL_0071-0098.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -410,8 +410,8 @@ m2lims=[[230,330,0,80]],
 m3lims=None)
 
 #file names for field O
-file25='F0590_FO_IMA_07018926_FORF253_CAL_0057-0086_Field_O.fits'
-file37='F0590_FO_IMA_07018926_FORF371_CAL_0057-0086_Field_O.fits'
+file25='F0590_FO_IMA_07018926_FORF253_CAL_0057-0086.fits'
+file37='F0590_FO_IMA_07018926_FORF371_CAL_0057-0086.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -425,8 +425,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field P
-file25='F0589_FO_IMA_07018927_FORF253_CAL_0138-0169_Field_P.fits'
-file37='F0589_FO_IMA_07018927_FORF371_CAL_0138-0169_Field_P.fits'
+file25='F0589_FO_IMA_07018927_FORF253_CAL_0138-0169.fits'
+file37='F0589_FO_IMA_07018927_FORF371_CAL_0138-0169.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -440,8 +440,8 @@ m2lims=[[350,380,0,370]],
 m3lims=None)
 
 #file names for field Q
-file25='F0590_FO_IMA_07018928_FORF253_CAL_0024-0055_Field_Q.fits'
-file37='F0590_FO_IMA_07018928_FORF371_CAL_0024-0055_Field_Q.fits'
+file25='F0590_FO_IMA_07018928_FORF253_CAL_0024-0055.fits'
+file37='F0590_FO_IMA_07018928_FORF371_CAL_0024-0055.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -455,8 +455,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field R
-file25='F0589_FO_IMA_07018929_FORF253_CAL_0080-0106_Field_R.fits'
-file37='F0589_FO_IMA_07018929_FORF371_CAL_0080-0106_Field_R.fits'
+file25='F0589_FO_IMA_07018929_FORF253_CAL_0080-0106.fits'
+file37='F0589_FO_IMA_07018929_FORF371_CAL_0080-0106.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -470,8 +470,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field S
-file25='F0592_FO_IMA_07018930_FORF253_CAL_0037-0067_Field_S.fits'
-file37='F0592_FO_IMA_07018930_FORF371_CAL_0037-0067_Field_S.fits'
+file25='F0592_FO_IMA_07018930_FORF253_CAL_0037-0067.fits'
+file37='F0592_FO_IMA_07018930_FORF371_CAL_0037-0067.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -485,8 +485,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field T
-file25='F0589_FO_IMA_07018931_FORF253_CAL_0108-0137_Field_T.fits'
-file37='F0589_FO_IMA_07018931_FORF371_CAL_0108-0137_Field_T.fits'
+file25='F0589_FO_IMA_07018931_FORF253_CAL_0108-0137.fits'
+file37='F0589_FO_IMA_07018931_FORF371_CAL_0108-0137.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -500,8 +500,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field U
-file25='F0591_FO_IMA_07018932_FORF253_CAL_0062-0087_Field_U.fits'
-file37='F0591_FO_IMA_07018932_FORF371_CAL_0062-0087_Field_U.fits'
+file25='F0591_FO_IMA_07018932_FORF253_CAL_0062-0087.fits'
+file37='F0591_FO_IMA_07018932_FORF371_CAL_0062-0087.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -515,8 +515,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field V
-file25='F0593_FO_IMA_07018933_FORF253_CAL_0039-0070_Field_V.fits'
-file37='F0593_FO_IMA_07018933_FORF371_CAL_0039-0070_Field_V.fits'
+file25='F0593_FO_IMA_07018933_FORF253_CAL_0039-0070.fits'
+file37='F0593_FO_IMA_07018933_FORF371_CAL_0039-0070.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -530,8 +530,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field W
-file25='F0593_FO_IMA_07018934_FORF253_CAL_0005-0038_Field_W.fits'
-file37='F0593_FO_IMA_07018934_FORF371_CAL_0005-0038_Field_W.fits'
+file25='F0593_FO_IMA_07018934_FORF253_CAL_0005-0038.fits'
+file37='F0593_FO_IMA_07018934_FORF371_CAL_0005-0038.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -545,8 +545,8 @@ m2lims=None,
 m3lims=None)
 
 #file names for field X
-file25='F0594_FO_IMA_07018937_FORF253_CAL_0018-0047_Field_X.fits'
-file37='F0594_FO_IMA_07018937_FORF371_CAL_0018-0047_Field_X.fits'
+file25='F0594_FO_IMA_07018937_FORF253_CAL_0018-0047.fits'
+file37='F0594_FO_IMA_07018937_FORF371_CAL_0018-0047.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -560,8 +560,8 @@ m2lims=[[0,40,0,340],[0,100,300,340]],
 m3lims=None)
 
 #file names for field Y
-file25='F0594_FO_IMA_07018938_FORF253_CAL_0048-0075_Field_Y.fits'
-file37='F0594_FO_IMA_07018938_FORF371_CAL_0048-0075_Field_Y.fits'
+file25='F0594_FO_IMA_07018938_FORF253_CAL_0048-0075.fits'
+file37='F0594_FO_IMA_07018938_FORF371_CAL_0048-0075.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -576,8 +576,8 @@ m3lims=None)
 
 #other fields from earlier cycles
 #File names for H north
-file25='F0217_FO_IMA_70030015_FORF253_CAL_0032-0057_Hnorth.fits'
-file37='F0217_FO_IMA_70030016_FORF371_CAL_0058-0071_Hnorth.fits'
+file25='F0217_FO_IMA_70030015_FORF253_CAL_0032-0057.fits'
+file37='F0217_FO_IMA_70030016_FORF371_CAL_0058-0071.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -591,8 +591,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for H South
-file25='F0217_FO_IMA_70030012_FORF253_CAL_0084-0093_Hsouth.fits'
-file37='F0217_FO_IMA_70030013_FORF371_CAL_0094-0100_Hsouth.fits'
+file25='F0217_FO_IMA_70030012_FORF253_CAL_0084-0093.fits'
+file37='F0217_FO_IMA_70030013_FORF371_CAL_0094-0100.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -606,8 +606,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for ArchE
-file25='F0225_FO_IMA_70030026_FORF253_CAL_0027-0044_ArchE.fits'
-file37='F0225_FO_IMA_70030028_FORF371_CAL_0062-0077_ArchE.fits'
+file25='F0225_FO_IMA_70030026_FORF253_CAL_0027-0044.fits'
+file37='F0225_FO_IMA_70030028_FORF371_CAL_0062-0077.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -621,8 +621,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for ArchNE
-file25='F0225_FO_IMA_70030023_FORF253_CAL_0078-0095_ArchNE.fits'
-file37='F0324_FO_IMA_70040067_FORF371_CAL_0030-0108_ArchNE.fits'
+file25='F0225_FO_IMA_70030023_FORF253_CAL_0078-0095.fits'
+file37='F0324_FO_IMA_70040067_FORF371_CAL_0030-0108.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -636,8 +636,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for ArchNW
-file25='F0224_FO_IMA_70030020_FORF253_CAL_0033-0056_ArchNW.fits'
-file37='F0224_FO_IMA_70030021_FORF371_CAL_0058-0080_ArchNW.fits'
+file25='F0224_FO_IMA_70030020_FORF253_CAL_0033-0056.fits'
+file37='F0224_FO_IMA_70030021_FORF371_CAL_0058-0080.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -651,8 +651,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for ArchSE
-file25='F0227_FO_IMA_70030029_FORF253_CAL_0059-0077_ArchSE.fits'
-file37='F0227_FO_IMA_70030030_FORF371_CAL_0078-0097_ArchSE.fits'
+file25='F0227_FO_IMA_70030029_FORF253_CAL_0059-0077.fits'
+file37='F0227_FO_IMA_70030030_FORF371_CAL_0078-0097.fits'
 if wavelength==25:
     fname=file25
 else:
@@ -667,8 +667,8 @@ m2lims=None,
 m3lims=None)
 
 #File Names for ArchW
-file25='F0225_FO_IMA_70030017_FORF371_CAL_0004-0026_ArchW.fits'
-file37='F0225_FO_IMA_70030017_FORF253_CAL_0004-0026_ArchW.fits'
+file25='F0225_FO_IMA_70030017_FORF371_CAL_0004-0026.fits'
+file37='F0225_FO_IMA_70030017_FORF253_CAL_0004-0026.fits'
 if wavelength==25:
     fname=file25
 else:
